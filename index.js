2c2ed150a4e634dec2f4b68268c1d1204df9d35f
abkux <kumarjhaabhishek2002@gmail.com>
2023-11-16 23:20:36 +0530

    Added more imports

--- index.js
+++ index.js
@@ -3,13 +3,20 @@ import bodyParser from 'body-parser';
 import cors from 'cors';
 import dotenv from 'dotenv';
 import path from 'path';
+import helmet from 'helmet'; 
 import { home } from './controller/home.js';
 
 const webserver = express();
 dotenv.config();
+webserver.use(express.json());
+webserver.use(helmet());
+webserver.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin'} ));
+webserver.use(morgan("common"));
+webserver.use(bodyParser.json({ limit: "50mb", extended: true }));
+webserver.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
+webserver.use(cors());
 
 webserver.get('/', home);
-
 
 const PORT = process.env.PORT || 3000;
 webserver.listen(PORT, () => {
