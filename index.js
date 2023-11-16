c154b662b08415c5f641ccd312fe1e153970cb1e
abkux <kumarjhaabhishek2002@gmail.com>
2023-11-16 23:07:23 +0530

    Second router

--- index.js
+++ index.js
@@ -10,6 +10,14 @@ webserver.get('/', async(req, res) => {
   }
 })
 
+webserver.get('/info', async(req, res) => {
+  try {
+    res.status(200).json({"name": "a1", "hosted_by": "Azure", "domain": "a1.abku.dev"})
+  } catch (error) {
+    res.status(500).json({"error_message": error.message})
+  }
+})
+
 
 webserver.listen(3000, () => {
   console.log('Server started on port 3000');
